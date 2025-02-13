3d8c7dfa213f66a490a047e1daa317706d3ad587
Dmitry Polyukhovich <dm.pol@yahoo.com>
2025-02-13 14:44:49 +0300

    Add healthbar

--- scripts/healthbar.gd
+++ scripts/healthbar.gd
@@ -1,4 +1,22 @@
+@tool
 extends Node
 
+@onready var current_skin := $AllySkin
+
+@export var value := 100.0:
+	set(val):
+		value = val
+		if is_node_ready():
+			current_skin.value = val
+
+@export var enemy_skin := false:
+	set(is_enemy):
+		enemy_skin = is_enemy
+		if is_node_ready():
+			current_skin.visible = false;
+			current_skin = $EnemySkin if is_enemy else $CompanionSkin
+			current_skin.value = value
+			current_skin.visible = true;
+
 func _on_health_changed(val):
-	print(val)
+	value = val
